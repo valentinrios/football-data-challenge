f13354511674386bbd5b9ef87e1af914d3d50904
Valentin Rios <vrios2504@gmail.com>
2022-06-25 16:28:24 -0500

    competition collection

--- app/server.js
+++ app/server.js
@@ -4,10 +4,30 @@ const express = require("express");
 var { graphqlHTTP } = require("express-graphql");
 var { buildSchema } = require("graphql");
 const axios = require('axios').default;
+const MongoClient = require('mongodb').MongoClient
+
 
 // Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
+
+let db, competition, team, player
+MongoClient.connect('mongodb://root:example@santex-football-data-mongo:27017/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+},
+  async (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db("santex-football-data-database")
+    competition = db.collection("competition")
+    competition.createIndex({"code":1})
+    team = db.collection("team")    
+    player = db.collection("player")
+
+  })
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -40,28 +60,31 @@ var root = {
       population: 30000000,
     };
   },
-  importLeague: () => {
-    let info;
+  importLeague: (req) => {
     var config = {
       method: 'get',
-      url: 'https://api.football-data.org//v2/competitions/PL',
-      headers: { 
+      url: `https://api.football-data.org/v2/competitions/${req.leagueCode}`,
+      headers: {
         'X-Auth-Token': 'c3ee902739e54d01920337af7edfc9dd'
       }
     };
-    
+
     return axios(config)
-    .then(function (response) {
-      return {
-        name: response.data.name,
-        code: response.data.code,
-        areaName: response.data.area.name
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });    
-    
+      .then(function (response) {
+        let competitionRetrieved = {
+          name: response.data.name,
+          code: response.data.code,
+          areaName: response.data.area.name
+        }
+      
+        competition.findOneAndUpdate({ "code": competitionRetrieved.code }, { $set: competitionRetrieved}, { upsert: true, returnNewDocument: true })
+
+        return competitionRetrieved
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
   },
 };
 
@@ -76,8 +99,7 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  
-  res.send("AAAAHeasdsdfsdfaa  aqqq llo Wo rl  d 334538dddfdf88   5 aaaa");
+  res.send("Welcome to Santex");
 });
 
 app.listen(PORT, HOST);
